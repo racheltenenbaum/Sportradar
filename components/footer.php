766aefa39f129b3c8a5cb55ff4b71c7062febeed
racheltenenbaum <racheltenenbaum14@gmail.com>
2024-12-01 00:42:59 +0100

    added header and footer

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+    </div>
+    <footer class="bg-body-tertiary text-center text-lg-start mt-5">
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © Sportradar Event Schedule by
+        <a class="text-body" href="https://github.com/racheltenenbaum">Rachel Tenenbaum</a>
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://kit.fontawesome.com/8ae3362ae0.js" crossorigin="anonymous"></script>
+  <title>Sportradar Event Schedule</title>
+</head>
+<body>
+  <nav class="navbar fixed-top navbar-expand-lg" style="position: sticky;
+    top: 0;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="Sportradar-Brand-Line_Color_White.svg" width="200px" class="d-inline-block ms-3">
+      </a>
+      <div class="collapse navbar-collapse" >
+        <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+          <li class='nav-item'>
+            <a href='index.php' class='nav-link'>Event Schedule</a>
+          </li><li class='nav-item'>
+            <a href='create_event.php' class='nav-link'>Add New Event</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Log In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="flex-wrapper">
+    <div class="container">
