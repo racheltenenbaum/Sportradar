e532687daac3f5383ff52f0690d3b3d967b620fe
racheltenenbaum <racheltenenbaum14@gmail.com>
2024-12-01 14:49:00 +0100

    added time filter to filtering form

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
   $rows_sports = getSports();
   $events = eventIndex();
 
-  if (isset($_POST["sport_filter"])) {
-    header("refresh: 0; url= 'event_results.php?search={$_POST["sport"]}'");
+  // event filter passing through the url
+  if (isset($_POST["filter"])) {
+    header("refresh: 0; url= 'event_results.php?sport={$_POST["sport"]}&start_date={$_POST["start_date"]}&end_date={$_POST["end_date"]}'");
   }
-
 
 ?>
 <?php include "components/header.php" ?>
@@ -16,16 +16,32 @@
 <div class="main-div">
   <div class="d-flex mb-4" style="justify-content: space-between;">
     <h2>Upcoming Events</h2>
-    <form method="post" class="d-flex">
-      <select class="form-select" name="sport">
-        <option value='All Sports'>All Sports</option>";
-        <?php
-        foreach ($rows_sports as $key => $row_sport) {
-          echo "<option value='{$row_sport["name"]}'>{$row_sport["name"]}</option>";
-        } ?>
-      </select>
-      <input type="submit" class="btn btn-primary ms-2" value="filter" name="sport_filter">
-    </form>
+    <div>
+      <form method="post">
+        <div class="d-flex">
+          <div>
+            <select class="form-select form-field" name="sport">
+              <option value='All Sports'>All Sports</option>";
+              <?php
+              foreach ($rows_sports as $key => $row_sport) {
+                echo "<option value='{$row_sport["name"]}'>{$row_sport["name"]}</option>";
+              } ?>
+            </select>
+            <div class="d-flex" style="justify-content: space-between;">
+              <label for="start_time">From: </label><br>
+              <input type="datetime-local" class="form-control ms-2 mt-2 form-field" style="width:130px" name="start_date" placeholder="Date & Time">
+            </div>
+            <div class="d-flex" style="justify-content: space-between;">
+              <label for="start_time">Until: </label><br>
+              <input type="datetime-local" class="form-control ms-2 mt-2 form-field" style="width:130px" name="end_date" placeholder="Date & Time">
+            </div>
+          </div>
+          <div>
+            <input type="submit" class="btn btn-primary ms-2" value="filter" style="height:100%" name="filter">
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <?php foreach ($events as $key => $event):?>
